9aa59c484ec23b0207cd10d3fceb253dc81a5795
Hoger Shoresh <hogers@genstudents.org>
2024-09-18 12:19:51 +0100

    Added initial project code

---
+++ project.py
@@ -0,0 +1,35 @@
+# Welcome message
+
+# Have options to choose from, Collection, Delivery, Employees Only
+
+    # if Collection chosen, Menu, Books
+             # Options Drinks, Food
+                    # Option Drinks will list drinks
+                    # Option Food will list foods
+        # Whatever drink or foods chosen, to store their options.
+            # Option Books will list books
+    # prompt to enter Name
+
+
+    # Option Delivery, will show Menu, Books
+            #  Options Drinks, Food
+                    # Option Drinks will list drinks
+                    # Option Food will list foods
+        # Whatever drink or foods chosen, to store their options.
+            # Option Books will list books
+    # prompt to enter name and address
+
+    # Employees Only
+    # Option shows password
+
+prompt = "enter password"
+
+message = input(prompt)
+
+while
+if message == "cheese":
+    print("Success")
+else:
+    print("Access denied")
+
+            
